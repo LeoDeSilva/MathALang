d97cf37b4aef113590053f4b32ca3bf0001b1c6a
leoDesilva <46300158+leoDesilva@users.noreply.github.com>
2021-12-02 20:33:29 +0000

    strings + identifiers + brackets ... basically completed the lexer

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -29,9 +29,16 @@ class Lexer:
             tokens.append(token)
             self.advance()
         return tokens
+
+
+    def eat_whitespace(self):
+        while self.char.isspace():
+            self.advance()
     
         
     def lex_char(self):
+        self.eat_whitespace()
+
         if self.char == "+":
             return Token(ADD,self.char)
         elif self.char == "-":
@@ -44,14 +51,67 @@ class Lexer:
             return Token(MOD,self.char)
         elif self.char == "^":
             return Token(POW,self.char)
+
+        elif self.char == ";":
+            return Token(SEMICOLON,self.char)
+        elif self.char == ":":
+            return Token(COLON,self.char)
+        elif self.char == "\\":
+            return Token(BACKSLASH,self.char)
+        elif self.char == ".":
+            return Token(DOT,self.char)
+        elif self.char == ",":
+            return Token(COMMA,self.char)
+
+        elif self.char == "(":
+            return Token(LPAREN,self.char)
+        elif self.char == ")":
+            return Token(RPAREN,self.char)
+        elif self.char == "[":
+            return Token(LSQUARE,self.char)
+        elif self.char == "]":
+            return Token(RSQUARE,self.char)
+        elif self.char == "{":
+            return Token(LBRACE,self.char)
+        elif self.char == "}":
+            return Token(RBRACE,self.char)
+        
+        elif self.char == '"':
+            return Token(STRING,self.lex_string())
+
         elif self.char.isnumeric():
             return Token(INT,self.lex_numeric())
+
+        elif self.char in LETTERS:
+            return Token(IDENTIFIER,self.lex_identifier())
+
+    
+    def lex_string(self):
+        self.advance()
+
+        start_pos = self.position 
+        while self.char != '"':
+            self.advance()
+
+        return self.file[start_pos:self.position]
+
+
+    def lex_identifier(self):
+        start_pos = self.position
+        while self.next != -1 and self.char in LETTERS:
+            self.advance()
+
+        return self.retreat_and_return(start_pos)
+    
     
     def lex_numeric(self):
         start_pos = self.position
         while self.next != -1 and self.char.isnumeric():
             self.advance()
 
-        int = self.file[start_pos:self.position]
+        return self.retreat_and_return(start_pos)
+
+    def retreat_and_return(self, start_pos):
+        value = self.file[start_pos:self.position]
         self.retreat()
-        return int
+        return value
